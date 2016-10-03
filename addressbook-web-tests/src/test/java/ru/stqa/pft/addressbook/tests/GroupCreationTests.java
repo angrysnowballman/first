8beb89d7c7d820c4c7da06db3cd3b9773f1faf53
angrysnowballman <angrysnowballman@gmail.com>
2016-10-03 23:25:23 +0300

    Применено делегирование
    Созданы пакеты appmanager и test. Делегирование на Appmanager

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.addressbook.tests;
+
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+
+public class GroupCreationTests extends TestBase {
+
+    @Test
+    public void testGroupCreation() {
+        app.gotoGroupPage();
+        app.initGroupCreation();
+        app.fillGroupForm(new GroupData("third", "test1", "test2"));
+        app.submitGroupCreation();
+        app.returntoGroupPage();
+    }
+
+}
