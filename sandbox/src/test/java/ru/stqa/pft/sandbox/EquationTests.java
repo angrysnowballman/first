959e782c1984424c764dee06697cbb3f058f1ea0
angrysnowballman <angrysnowballman@gmail.com>
2016-10-25 23:21:14 +0300

    тесты по дискриминанту

--- sandbox/src/test/java/ru/stqa/pft/sandbox/EquationTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/EquationTests.java
@@ -32,4 +32,27 @@ public class EquationTests {
 
 
     }
+
+    @Test
+    public  void testLinear () {
+        Equation e = new Equation(0,1,1);
+        Assert.assertEquals(e.rootNumber(),1);
+
+
+    }
+
+    @Test
+    public  void testConstant () {
+        Equation e = new Equation(0,0,1);
+        Assert.assertEquals(e.rootNumber(),0);
+
+
+    }
+    @Test
+    public  void testZero () {
+        Equation e = new Equation(0,0,0);
+        Assert.assertEquals(e.rootNumber(),-1);
+
+
+    }
 }
