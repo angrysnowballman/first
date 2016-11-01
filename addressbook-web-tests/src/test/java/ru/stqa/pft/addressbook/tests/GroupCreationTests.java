48705398163651e1bf43efa7b6ccede26d15db74
angrysnowballman <angrysnowballman@gmail.com>
2016-11-01 22:03:24 +0300

    не работает

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -8,10 +8,10 @@ import ru.stqa.pft.addressbook.model.GroupData;
 public class GroupCreationTests extends TestBase {
 
     @Test
-    public void testGroupCreation() {
+    public void  testGroupCreation() {
         app.getNavigationHelper().gotoGroupPage();
         app.getGroupHelper().initGroupCreation();
-        app.getGroupHelper().fillGroupForm(new GroupData("third", "test1", "test2"));
+        app.getGroupHelper().fillGroupForm(new GroupData("test1", null, null ));
         app.getGroupHelper().submitGroupCreation();
         app.getGroupHelper().returntoGroupPage();
     }
