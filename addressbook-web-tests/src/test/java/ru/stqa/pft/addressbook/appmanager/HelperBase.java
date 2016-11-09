118592a8eb87ab991f3d601a931a27cfc383f436
angrysnowballman <angrysnowballman@gmail.com>
2016-11-09 09:33:33 +0300

    точка, дебагер

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -22,8 +22,12 @@ public class HelperBase {
     protected void type(By locator, String text) {
         click(locator);
         if (text != null) {
-            wd.findElement(locator).clear();
-            wd.findElement(locator).sendKeys(text);
+            String exsistingText = wd.findElement(locator).getAttribute("value");
+            if (! text.equals(exsistingText)) {
+                wd.findElement(locator).clear();
+                wd.findElement(locator).sendKeys(text);
+            }
+
         }
 
     }
