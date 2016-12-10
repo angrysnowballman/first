fcf286cde173c0c6bd03c34187602403570b117b
angrysnowballman <angrysnowballman@gmail.com>
2016-12-10 22:31:31 +0300

    создан рабочий класс создания контакта

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactDaa.java
@@ -0,0 +1,49 @@
+package ru.stqa.pft.addressbook.model;
+
+public class ContactDaa {
+    private final String first_name;
+    private final String last_name;
+    private final String adress;
+    private final String home_number;
+    private final String mobile;
+    private final String year;
+    private final String email;
+
+    public ContactDaa(String first_name, String Last_name, String adress, String home_number, String mobile, String year, String email) {
+        this.first_name = first_name;
+        last_name = Last_name;
+        this.adress = adress;
+        this.home_number = home_number;
+        this.mobile = mobile;
+        this.year = year;
+        this.email = email;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public String getHome_number() {
+        return home_number;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -0,0 +1,82 @@
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.*;
+import ru.stqa.pft.addressbook.model.ContactDaa;
+
+public class ContactCreationTests {
+    FirefoxDriver wd;
+    
+    @BeforeMethod
+    public void setUp() throws Exception {
+        wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        wd.get("http://localhost/addressbookv4.1.4/");
+    }
+    
+    @Test
+    public void testContactCreation() {
+        gotoAddContact();
+        fillContact(new ContactDaa("Petr", "Mushroom", "Tverskaya st.68", "84954589565", "84956235151", "1964", "test@inbox_1.ru"));
+        submitContactCreation();
+    }
+
+    private void submitContactCreation() {
+        wd.findElement(By.name("submit")).click();
+    }
+
+    private void fillContact(ContactDaa contactDaa) {
+        wd.findElement(By.name("firstname")).click();
+        wd.findElement(By.name("firstname")).clear();
+        wd.findElement(By.name("firstname")).sendKeys(contactDaa.getFirst_name());
+        wd.findElement(By.name("lastname")).click();
+        wd.findElement(By.name("lastname")).clear();
+        wd.findElement(By.name("lastname")).sendKeys(contactDaa.getLast_name());
+        wd.findElement(By.name("address")).click();
+        wd.findElement(By.name("address")).clear();
+        wd.findElement(By.name("address")).sendKeys(contactDaa.getAdress());
+        wd.findElement(By.name("home")).click();
+        wd.findElement(By.name("home")).clear();
+        wd.findElement(By.name("home")).sendKeys(contactDaa.getHome_number());
+        wd.findElement(By.name("mobile")).click();
+        wd.findElement(By.name("mobile")).clear();
+        wd.findElement(By.name("mobile")).sendKeys(contactDaa.getMobile());
+        wd.findElement(By.name("mobile")).click();
+        wd.findElement(By.name("mobile")).clear();
+        wd.findElement(By.name("mobile")).sendKeys("89165422654");
+        if (!wd.findElement(By.xpath("//div[@id='content']/form/select[1]//option[5]")).isSelected()) {
+            wd.findElement(By.xpath("//div[@id='content']/form/select[1]//option[5]")).click();
+        }
+        if (!wd.findElement(By.xpath("//div[@id='content']/form/select[2]//option[8]")).isSelected()) {
+            wd.findElement(By.xpath("//div[@id='content']/form/select[2]//option[8]")).click();
+        }
+        wd.findElement(By.name("byear")).click();
+        wd.findElement(By.name("byear")).clear();
+        wd.findElement(By.name("byear")).sendKeys(contactDaa.getYear());
+        wd.findElement(By.name("email")).click();
+        wd.findElement(By.name("email")).clear();
+        wd.findElement(By.name("email")).sendKeys(contactDaa.getEmail());
+    }
+
+    private void gotoAddContact() {
+        wd.findElement(By.linkText("add new")).click();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        wd.quit();
+    }
+    
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+}
